240de7880cc369edf02a27beec27e046c3cb0888
Mark Ryan <39224115+ryanmark1867@users.noreply.github.com>
2021-10-01 21:02:15 -0400

    Add files via upload

---
+++ hello_world.cbl
@@ -0,0 +1,11 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO-WORLD.
+       PROCEDURE DIVISION.
+       DISPLAY 'hello world'.
+       STOP RUN.
